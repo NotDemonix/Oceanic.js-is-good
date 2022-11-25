29e9c9d54e4f2e0f93fd1964761f02fa7829bdbd
NotDemonix <90858555+NotDemonix@users.noreply.github.com>
2022-11-25 14:28:17 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
-const {Client} = require ('oceanic.js');
+const {Client, ApplicationCommandTypes} = require ('oceanic.js');
 
 const client = new Client({
     auth: 'Bot <token>'
 });
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log('Oceanic.js is giga')
+
+    await client.application.bulkEditGlobalCommands([
+        {
+            dmPermission: true,
+            type: ApplicationCommandTypes.CHAT_INPUT,
+            name: 'ping',
+            description: 'Check bot latency',
+        },
+    ])
 })
 
 client.on('interactionCreate', (interaction) => {
-    //
+    if(interaction.data.name == 'ping') {
+        interaction.createMessage({ content: Math.round(interaction.guild.latency), flags: 64 });
+    }
 })
 client.on('messageCreate', (message) => {
     if(message.content.startsWith('!ping') {
         message.channel.createMessage({
             content: Math.round(message.guild.latency)
-        })
+        });
     }
 })
 
